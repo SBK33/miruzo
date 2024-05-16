c1eb3dff86ae97d611a0004241bf145119d352a5
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-05-16 14:51:33 +0000

    posts_controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,16 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: "homes#top"
     get 'homes/about'
+    
+    
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/new'
+    get 'posts/create'
+    get 'posts/edit'
+    get 'posts/update'
+    get 'posts/destroy'
+    
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
