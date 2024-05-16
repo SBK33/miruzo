8204001a3d96737abc2f11a0ce54a2b2a60a61cc
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-05-16 16:15:57 +0000

    Bootstrapの準備

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+
+#地図表示に関する記述
+  validates :address, presence: true
+
+  geocoded_by :address
+  after_validation :geocode
+#geocoded_by :addressで、addressカラムの内容を緯度・経度に変換することを指定しています。
+#after_validation :geocodeで、バリデーションの実行後に変換処理を実行して、latitudeカラム・longitudeカラムに緯度・経度の値が入力されます。
+#住所入力周りの機能不要の可能性あり。要見直し
 end
