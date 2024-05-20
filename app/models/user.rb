0ede4726024592eb30ffcf9d6767b9c1c670b5de
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-05-20 14:59:28 +0000

    userモデルに画像保存機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,13 @@ class User < ApplicationRecord
 
 #アソシエーション設定
   has_many :posts, dependent: :destroy
-
-
+#画像の添付設定
+  has_one_attached :profile_image
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample_user.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
