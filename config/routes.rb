c138fe972486292d9fd0700fb206c1dbf3fce27e
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-05-21 15:34:30 +0000

    ヘッダーリンク編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-  end
+
   devise_for :users
   scope module: :public do
     root to: "homes#top"
-    get 'homes/about'
+    get 'homes/about' => "homes#about", as: "about"
 
     resources :posts, only: [:new, :index, :show, :create, :edit, :update, :destroy]
     #↑onlyにしなくてよい可能性あり。要確認。
+    resources :users, only: [:show, :edit]
 
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
