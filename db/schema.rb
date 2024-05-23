bafdc0bbf1b998fd4f020ffe85a55d99e412cd91
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-05-23 15:28:19 +0000

    地図投稿用カラムをpostsmodelに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_22_151604) do
+ActiveRecord::Schema.define(version: 2024_05_23_152611) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,9 @@ ActiveRecord::Schema.define(version: 2024_05_22_151604) do
     t.boolean "is_private_user"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address", default: "", null: false
+    t.float "latitude", default: 0.0, null: false
+    t.float "longitude", default: 0.0, null: false
   end
 
   create_table "users", force: :cascade do |t|
