2f858a15fabc087f2f172912d6612f660656661c
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-06-29 06:56:59 +0000

    検索フォーム部分テンプレート新規作成、searchsコントローラ新規作成、検索用ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,12 @@ Rails.application.routes.draw do
     resources :posts, only: [:new, :index, :show, :create, :edit, :update, :destroy] do
       resources :comments, only: [:create, :destroy]
     end
-    
+
     #↑onlyにしなくてよい可能性あり。要確認。
     resources :users, only: [:show, :edit, :update]
 
+    #検索用ルーティング
+    get "search" => "searches#search"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
