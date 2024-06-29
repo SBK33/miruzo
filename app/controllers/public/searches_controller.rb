e59cac992310fd51c252650594d82ece34ec54d0
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-06-29 10:38:13 +0000

    検索結果view作成

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,11 +1,11 @@
 class Public::SearchesController < ApplicationController
   def search
     @range = params[:range]
-
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
     else
       @posts = Post.looks(params[:search], params[:word])
     end
+#      redirect_to search_path
   end
 end
