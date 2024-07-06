24ba9f9e00373929e63bd7cc99615ffa6c922881
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-07-06 15:42:35 +0000

    admin用ログイン以外のdevice関連機能削除・ログイン/ログアウト後遷移先設定

--- app/controllers/admin/confirmations_controller.rb
+++ app/controllers/admin/confirmations_controller.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class Admin::ConfirmationsController < Devise::ConfirmationsController
+  protected
+
+  def after_sign_in_path_for(resource)
+    admin_dashboards_path # ログイン後にリダイレクトするパス
+  end
+
+   def after_sign_out_path_for(resource_or_scope)
+    new_admin_session_path # ログアウト後にリダイレクトするパス
+  end
+  
   # GET /resource/confirmation/new
   # def new
   #   super
